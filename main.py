f72c4f78119b744ebdd0b48e678b822c117c18d5
hardik somani <hardiksomani659@gmail.com>
2025-08-19 02:25:14 +0530

    prepare_base_model_added

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from src.cnnclassifier import logger
 
-logger.info("Welcome to my personal log")
+# logger.info("Welcome to my personal log")
+
+
+
+from src.cnnclassifier import logger
+from src.cnnclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
