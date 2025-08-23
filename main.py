d066a1adb17b08dc568904c54ec56a25c2a2e58e
hardik somani <hardiksomani659@gmail.com>
2025-08-23 17:49:21 +0530

    upto model_evaluation updated

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from src.cnnclassifier import logger
 
 from src.cnnclassifier import logger
 from src.cnnclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnclassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnclassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from src.cnnclassifier.pipeline.stage_04_evaluation import EvaluationPipeline
+
+
 
 
 
@@ -19,3 +24,37 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+
+
+STAGE_NAME = "Prepare Base Model" 
+try: 
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<") 
+    prepare_base_model = PrepareBaseModelTrainingPipeline() 
+    prepare_base_model.main() 
+    logger.info(f">>>>>>> {STAGE_NAME} completed <<<<<\n\nx=======x") 
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Evaluation"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    model_eval = EvaluationPipeline()
+    model_eval.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/cnnclassifier/config/configuration.py
+++ src/cnnclassifier/config/configuration.py
@@ -1,8 +1,10 @@
 from src.cnnclassifier.constants import *
 from src.cnnclassifier.utils.common import read_yaml, create_directories
 from src.cnnclassifier.entity.config_entity import DataIngestionConfig
-# from src.cnnclassifier.entity.config_entity import PrepareBaseModelConfig
-# from src.cnnclassifier.entity.config_entity import PrepareCallbacksConfig
+from src.cnnclassifier.entity.config_entity import PrepareBaseModelConfig
+from src.cnnclassifier.entity.config_entity import PrepareCallbacksConfig
+from src.cnnclassifier.entity.config_entity import TrainingConfig
+from src.cnnclassifier.entity.config_entity import EvaluationConfig
 from pathlib import Path
 import os
 
@@ -30,34 +32,72 @@ class ConfigurationManager:
         )
     
 
-    # def get_prepare_base_model_config(self):
-    #     config = self.config['prepare_base_model'] 
-    #     params = self.params 
-    #     create_directories([config['root_dir']]) 
+    def get_prepare_base_model_config(self):
+        config = self.config['prepare_base_model'] 
+        params = self.params 
+        create_directories([config['root_dir']]) 
 
-    #     return PrepareBaseModelConfig(
-    #         root_dir= Path(config['root_dir']),
-    #         base_model_path=Path(config['base_model_path']),
-    #         updated_base_model_path=Path(config['updated_base_model_path']),
-    #         params_image_size=params['IMAGE_SIZE'],
-    #         params_learning_rate=params['LEARNING_RATE'],
-    #         params_include_top=params['INCLUDE_TOP'],
-    #         params_weights=params['WEIGHTS'],
-    #         params_classes=params['CLASSES'] 
-    #     )
+        return PrepareBaseModelConfig(
+            root_dir= Path(config['root_dir']),
+            base_model_path=Path(config['base_model_path']),
+            updated_base_model_path=Path(config['updated_base_model_path']),
+            params_image_size=params['IMAGE_SIZE'],
+            params_learning_rate=params['LEARNING_RATE'],
+            params_include_top=params['INCLUDE_TOP'],
+            params_weights=params['WEIGHTS'],
+            params_classes=params['CLASSES'] 
+        )
     
 
-    # def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
-    #     config = self.config['prepare_callbacks']
-    #     model_ckpt_dir = os.path.dirname(config['checkpoint_model_filepath'])
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config['prepare_callbacks']
+        model_ckpt_dir = os.path.dirname(config['checkpoint_model_filepath'])
 
-    #     create_directories([
-    #         Path(model_ckpt_dir),
-    #         Path(config['tensorboard_root_log_dir'])
-    #     ])
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config['tensorboard_root_log_dir'])
+        ])
 
-    #     return PrepareCallbacksConfig(
-    #         root_dir=Path(config['root_dir']),
-    #         tensorboard_root_log_dir=Path(config['tensorboard_root_log_dir']),
-    #         checkpoint_model_filepath=Path(config['checkpoint_model_filepath'])
-    #     )
+        return PrepareCallbacksConfig(
+            root_dir=Path(config['root_dir']),
+            tensorboard_root_log_dir=Path(config['tensorboard_root_log_dir']),
+            checkpoint_model_filepath=Path(config['checkpoint_model_filepath'])
+        )
+    
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config['training']
+        prepare_base_model = self.config['prepare_base_model']
+        params = self.params
+
+        training_data = os.path.join(
+            self.config['data_ingestion']['unzip_dir'],
+            "Chicken-fecal-images"
+        )
+
+        create_directories([Path(training['root_dir'])])
+
+        return TrainingConfig(
+            root_dir=Path(training['root_dir']),
+            trained_model_path=Path(training['trained_model_path']),
+            updated_base_model_path=Path(prepare_base_model['updated_base_model_path']),
+            training_data=Path(training_data),
+            params_epochs=params['EPOCHS'],
+            params_batch_size=params['BATCH_SIZE'],
+            params_is_augmentation=params['AUGMENTATION'],
+            params_image_size=params['IMAGE_SIZE'],
+            params_learning_rate=params['LEARNING_RATE'] 
+
+        )
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        return EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params['IMAGE_SIZE'],
+            params_batch_size=self.params['BATCH_SIZE']
+        )
+
+    
